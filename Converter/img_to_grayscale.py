29845c869bd859f6ca1ddfa33aa431b31b82e7eb
Javid Mahmudov <Jav1d@users.noreply.github.com>
2019-03-09 00:56:08 +0400

    First commit

--- Converter/img_to_grayscale.py
+++ Converter/img_to_grayscale.py
@@ -1,6 +1,32 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = np.array(Image.open("Images/iss.jpg"))
 
-print(img)
+plt.imshow(img)
+
+
+def rgb_to_gray(img):
+    grayImage = np.zeros(img.shape)
+    R = np.array(img[:, :, 0])
+    G = np.array(img[:, :, 1])
+    B = np.array(img[:, :, 2])
+
+    R = (R * .299)
+    G = (G * .587)
+    B = (B * .114)
+
+    avg = (R + G + B)
+    grayImage = img
+
+    for i in range(3):
+        grayImage[:, :, i] = avg
+
+    return grayImage
+
+
+gimg = rgb_to_gray(img.copy())
+plt.imshow(gimg)
+plt.show()
+
